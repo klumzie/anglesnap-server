1e9d68373f5afcfd357b3ed349cd2f93671cb840
klumzie <36917461+klumzie@users.noreply.github.com>
2025-07-23 12:43:21 +0100

    Update AngleSnapServer.java

--- src/main/java/com/AngleSnapServer/AngleSnapServer.java
+++ src/main/java/com/AngleSnapServer/AngleSnapServer.java
@@ -1,4 +1,23 @@
-@Override
-public void onInitialize() {
-    new ArrowTracker().register();
+package com.AngleSnapServer;
+
+import me.contaria.anglesnapserver.ArrowTracker;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AngleSnapServer implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("anglesnap-server");
+
+    /**
+     * This method runs once when the mod is first loaded.
+     */
+    @Override
+    public void onInitialize() {
+        // This is where you run your mod's setup code.
+        LOGGER.info("Initializing Angle Snap Server...");
+
+        // Register the arrow tracking logic.
+        new ArrowTracker().register();
+    }
 }
