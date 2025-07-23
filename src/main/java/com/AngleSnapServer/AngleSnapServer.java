129a9d7b01bba3aaf5401364654c3538438c61e4
klumzie <36917461+klumzie@users.noreply.github.com>
2025-07-23 12:41:15 +0100

    Update AngleSnapServer.java

--- src/main/java/com/AngleSnapServer/AngleSnapServer.java
+++ src/main/java/com/AngleSnapServer/AngleSnapServer.java
@@ -1,10 +1,4 @@
-package me.contaria.anglesnapserver;
-
-import net.fabricmc.api.ModInitializer;
-
-public class AngleSnapServer implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        new ArrowTracker().register();
-    }
+@Override
+public void onInitialize() {
+    new ArrowTracker().register();
 }
